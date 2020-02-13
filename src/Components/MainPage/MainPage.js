f510d4ae321e807efde4c7b702908536f5b4ce65
MZO9400 <hamza.hameed00@yahoo.com>
2020-02-13 20:15:21 +0500

    Make NavBar and footer global

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,27 +1,23 @@
 import React from "react";
 //import CSS from "./MainPage.module.css";
 import { withRouter } from "react-router-dom";
-import NavBar from "../../Containers/NavBar/NavBar";
 import FirstJumbotron from "./FirstJumbotron/FirstJumbotron";
 import SecondJumbotron from "./SecondJumbotron/SecondJumbotron";
 import ThirdJumbotron from "./ThirdJumbotron/ThirdJumbotron";
 import FourthJumbotron from "./FourthJumbotron/FourthJumbotron";
 import FifthJumbotron from "./FifthJumbotron/FifthJumbotron";
 import SixthJumbotron from "./SixthJumbotron/SixthJumbotron";
-import Footer from "../../Containers/Footer/Footer";
 
 class MainPage extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <NavBar />
         <FirstJumbotron />
         <SecondJumbotron />
         <ThirdJumbotron />
         <FourthJumbotron />
         <FifthJumbotron />
         <SixthJumbotron />
-        <Footer />
       </React.Fragment>
     );
   }
--- src/RouterContainer.js
+++ src/RouterContainer.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 import MainPage from "./Components/MainPage/MainPage";
+import NavBar from "./Containers/NavBar/NavBar";
+import Footer from "./Containers/Footer/Footer";
 
 class RouterContainer extends React.Component {
   render() {
     return (
-      <Switch>
-        <Route path="/" exact component={MainPage} />
-        <Redirect to="/" />
-      </Switch>
+      <React.Fragment>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={MainPage} />
+          <Redirect to="/" />
+        </Switch>
+        <Footer />
+      </React.Fragment>
     );
   }
 }
