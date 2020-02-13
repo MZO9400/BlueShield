fc2fd29c3f3fa816ed92e6a9f6dea9453a4f72ed
MZO9400 <hamza.hameed00@yahoo.com>
2020-02-13 20:45:35 +0500

    Privacy Policy placeholder

--- src/RouterContainer.js
+++ src/RouterContainer.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
 import MainPage from "./Components/MainPage/MainPage";
 import Disclaimers from "./Components/Disclaimers/Disclaimers";
+import PrivacyPolicy from "./Components/PrivacyPolicy/PrivacyPolicy";
 import NavBar from "./Containers/NavBar/NavBar";
 import Footer from "./Containers/Footer/Footer";
 
@@ -13,6 +14,7 @@ class RouterContainer extends React.Component {
         <Switch>
           <Route path="/" exact component={MainPage} />
           <Route path="/disclaimer" exact component={Disclaimers} />
+          <Route path="/privacy-policy" exact component={PrivacyPolicy} />
           <Redirect to="/" />
         </Switch>
         <Footer />
