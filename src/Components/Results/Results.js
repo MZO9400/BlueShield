f1bbb4dd8ce63ad180b9e97956c902309e75f0f6
MZO9400 <hamza.hameed00@yahoo.com>
2020-02-29 00:12:57 +0500

    Results screen

---
+++ src/Components/Results/Results.js
@@ -0,0 +1,62 @@
+import React from "react";
+import CompareSteps from "../../Components/CompareSteps/CompareSteps";
+import { FormGroup, Label, Input } from "reactstrap";
+import CSS from "./Results.module.css";
+import Axios from "../../AxiosInstance";
+import JubileeGeneral from "../../Images/jgi.png";
+class Results extends React.PureComponent {
+  render() {
+    return (
+      <React.Fragment>
+        <CompareSteps step={2} />
+        <div className={CSS.main}>
+          <div className={CSS.filters}>
+            <div>
+              <h6>Sort by</h6>
+              <FormGroup check>
+                <Label check>
+                  <Input type="radio" name="radio1" /> Recommended
+                </Label>
+              </FormGroup>
+
+              <FormGroup check>
+                <Label check>
+                  <Input type="radio" name="radio1" /> Cheapest
+                </Label>
+              </FormGroup>
+            </div>
+            <div className={CSS.barrier} />
+            <div>
+              <h6>Filter</h6>
+              <FormGroup check>
+                <Label check>
+                  <Input type="checkbox" /> Jubilee Insurance
+                </Label>
+              </FormGroup>
+
+              <FormGroup check>
+                <Label check>
+                  <Input type="checkbox" /> Allianz EFU Insurance
+                </Label>
+              </FormGroup>
+
+              <FormGroup check>
+                <Label check>
+                  <Input type="checkbox" /> TPL Life Insurance
+                </Label>
+              </FormGroup>
+
+              <FormGroup check>
+                <Label check>
+                  <Input type="checkbox" /> UIC Takaful Insurance
+                </Label>
+              </FormGroup>
+            </div>
+          </div>
+          <div className={CSS.resultcards}></div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+export default Results;
